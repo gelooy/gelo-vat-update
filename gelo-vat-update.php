2f052e585ccd0fd5dd9862671fdd4fd2ed6328fe
Tero Lahtinen <tero.lahtinen@gelo.fi>
2024-08-21 14:20:33 +0300

    must use wp_schdule_single_event instead of wp_schedule_event

--- gelo-vat-update.php
+++ gelo-vat-update.php
@@ -3,13 +3,13 @@
 Plugin Name: Gelo VAT Update
 Plugin URI: https://gelo.fi/
 Description: A plugin to automatically update tax rate from 24 to 25.5 at Sep 1st 00.00.00 2024. The plugin does once-in-a-lifetime update and changes updates FI tax with value 24 to 25.5
-Version: 1.0.0
+Version: 1.0.1
 Author: Tero Lahtinen/Gelo
 Author URI: https://gelo.fi
 License: GPL2
 */
 
-define('GELO_VAT_UPDATE_TEST_MODE', false); // set to true to test the plugin, in test mode runs the update after one minute
+define('GELO_VAT_UPDATE_TEST_MODE', true); // set to true to test the plugin, in test mode runs the update after one minute
 
 register_activation_hook(__FILE__, 'gelo_vat_update_activate');
 register_deactivation_hook(__FILE__, 'gelo_vat_update_deactivate');
@@ -18,10 +18,10 @@ register_deactivation_hook(__FILE__, 'gelo_vat_update_deactivate');
 function gelo_vat_update_activate() {
     add_action('gelo_vat_update_event', 'gelo_vat_update_function');
     if (GELO_VAT_UPDATE_TEST_MODE) {
-        wp_schedule_event(strtotime('+1 minute'), '', 'gelo_vat_update_event');
+        wp_schedule_single_event(strtotime('+1 minute'), '', 'gelo_vat_update_event');
         return;
     }
-    wp_schedule_event(strtotime('2024-09-01 00:00:00'), '', 'gelo_vat_update_event');
+    wp_schedule_single_event(strtotime('2024-09-01 00:00:00'), '', 'gelo_vat_update_event');
 }
 
 
@@ -37,7 +37,7 @@ function gelo_vat_update_function() {
         array('%f'),
         array('%f', '%s')
     ); 
-    wp_clear_scheduled_hook('gelo_vat_update_event');   
+    remove_action('gelo_vat_update_event', 'gelo_vat_update_function');  
 }
 
 // Plugin deactivation callback
